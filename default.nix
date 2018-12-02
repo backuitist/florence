cf711df1dc03f1e775c072f60e2b31073f8e2bfc
Bruno Bieth <biethb@gmail.com>
2018-12-02 21:13:57 +0100

    add nix build

--- default.nix
+++ default.nix
@@ -4,7 +4,11 @@ let
 in
  nixpkgs.stdenv.mkDerivation {
    name = "florence-${version}";
-   src = ".";
+   src = fetchGit ./.;
+   buildInputs = with nixpkgs; [ intltool pkgconfig glib cairo libxml2 # gnome-doc-utils gnome2.scrollkeeper
+                                 gst_all_1.gstreamer gnome3.librsvg gnome3.gtk libnotify
+                                 xorg.libXtst which libxslt ];
+   configureFlags = "--without-at-spi --without-docs";
    meta = {
      description = "A virtual keyboard";
    };
